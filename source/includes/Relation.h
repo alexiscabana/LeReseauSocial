c6be16eaf779d15485eec59163717c52c71d6514
Alexis Cabana <alex021994@gmail.com>
2014-10-21 03:17:10 +0000

    added fields in Relation definition

--- source/includes/Relation.h
+++ source/includes/Relation.h
@@ -10,10 +10,12 @@ class Relation
         enum RelationType {AMITIE, CONNAISSANCE, TRAVAIL, NB_REL};
         Relation(RelationType type=CONNAISSANCE):_type(type){ _startingDate = Date();};
         std::string getRelationType();
+        void accept(){isAccepted=true;};
 
     private:
         RelationType _type;
         Date _startingDate;
+        bool isAccepted=false;
 };
 
 #endif
